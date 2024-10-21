cc9468c2dd35dd5f23b794d3e070e8a054410029
Rumpa Giri <codekunoichi@users.noreply.github.com>
2024-10-21 03:02:49 +0000

    Add get sample visit for select onchange

--- app/app.py
+++ app/app.py
@@ -6,6 +6,9 @@ from fastapi.staticfiles import StaticFiles
 from app.model_orchestrator import ModelOrchestrator
 from app.visit_summary import VisitSummary, visit_1, visit_3, visit_2, visit_4, sdoh_visit, diagnosis_visit_note, lab_result
 from flask import render_template
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.visit_summary import VisitSummary
 
 import os
 from pathlib import Path
@@ -28,6 +31,24 @@ async def get_landing_page(request: Request):
 
 # Create the absolute path for the static directory
 static_dir = Path(__file__).parent / "templates/static"
+
+
+# Define a new route to get the visit note based on the selected option
+@app.get("/get_visit_note")
+def get_visit_note(note_id: str):
+    print(f"Note ID selected..{note_id}")
+    visit_notes = {
+        "visit_1": VisitSummary.get_visit_1(),
+        "visit_2": VisitSummary.get_visit_2(),
+        "visit_3": VisitSummary.get_visit_3(),
+        "visit_4": VisitSummary.get_visit_4(),
+        "sdoh_visit": VisitSummary.get_sdoh_visit(),
+        "diagnosis_visit_note": VisitSummary.get_diagnosis_visit_note(),
+        "lab_result": VisitSummary.get_lab_result_samplenote()
+    }
+    
+    note = visit_notes.get(note_id, "No visit note found.")
+    return JSONResponse({"visit_note": note})
 
 # Mount the static directory using the absolute path
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
